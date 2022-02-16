f6911e5cc353bca5593966ac91d4c79d9e3b8f23
Daniel Makarov <danielmakarov.dev@gmail.com>
2022-02-15 21:24:03 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address)
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to SoloPoketDAO! 🧑‍💻</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+  
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
 
 export default App;
